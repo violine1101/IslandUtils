81ac627aa2ae0a1a27ec917c1afe712e1ee60cb7
violine1101 <violine1101@gmail.com>
2025-11-02 00:11:14 +0100

    Release version ve.2025-11-02.b - fixing release workflow

--- release_version.sh
+++ release_version.sh
@@ -2,18 +2,19 @@
 VERSION=$(grep -oP '^mod_version=(.*)$' gradle.properties | sed 's/mod_version=//')
 CREATE_RELEASE=false
 if [ -n "$VERSION" ]; then
-  echo "Detected mod version $VERSION"
+  echo "# Detected mod version $VERSION\n"
   # Was able to extract version. Check if version already exists:
   EXISTING_TAG=$(git tag -l $VERSION)
   if [ -z "$EXISTING_TAG" ]; then
     # The tag doesn't exist yet; create a new one
     git tag $VERSION
     git push origin tag $VERSION
-    echo "Created tag $VERSION"
+    echo "# Created tag $VERSION\n"
     CREATE_RELEASE=true
   else
-    echo "Tag $VERSION already exists; skipping creation of release"
+    echo "# Tag $VERSION already exists; skipping creation of release\n"
   fi
 else
-  echo "No mod version detected"
+  echo "# No mod version detected\n"
 fi
+echo "CREATE_RELEASE=$CREATE_RELEASE"
